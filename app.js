039f04da4bef6fe1c2273a7f14eec3942a21f572
Tim Beattie <trbeattie@outlook.com>
2017-11-05 14:59:02 -0500

    Added mongoose connection to app.js

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var users = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost/mdn-express-tutorial';
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
