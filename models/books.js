777ca4d51e3270346d7bd69711d1c81d57ac6294
Tim Beattie <trbeattie@outlook.com>
2017-11-05 15:30:06 -0500

    added book schema

---
+++ models/books.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var BookSchema = Schema({
+  title: {type: String, required: true},
+  author: {type: Schema.ObjectId, ref: 'Author', required: true},
+  summary: {type: String, required: true},
+  isbn: {type: String, required: true},
+  genre: [{type: Schema.ObjectId, ref: 'Genre'}]
+});
+
+//Virtual for book's URL
+BookSchema.virtual('url').get(function(){
+  return 'catalog/books/' + this._id;
+});
+
+module.exports = mongoose.model('Book', BookSchema);
