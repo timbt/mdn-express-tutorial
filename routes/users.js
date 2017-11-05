331fac01ce76b9e2361f3679358e6fbb7117fc8b
Tim Beattie <trbeattie@outlook.com>
2017-11-04 21:17:12 -0400

    Changed response messge for GET /users and added route for /users/:userId

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('index of users');
+});
+
+router.get('/:userId/', function(req ,res, next){
+  res.send('User ID: ' + req.params.userId);
 });
 
 module.exports = router;
