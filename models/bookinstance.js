42831a105b1f8399e31ec749da449bf92df9f353
Tim Beattie <trbeattie@outlook.com>
2017-11-11 14:30:53 -0500

    Added pretty due back dates for book instances using moment.js

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -14,6 +15,10 @@ var BookInstanceSchema = Schema({
 //Virtual for bookinstance's URL
 BookInstanceSchema.virtual('url').get(function(){
   return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function() {
+  return moment (this.due_back).format('MMMM Do, YYYY');
 });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
